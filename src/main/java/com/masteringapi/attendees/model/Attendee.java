355d082bcb39a561e82637e960f1d73e94e3de44
James Gough <jpgough@gmail.com>
2022-12-15 16:31:10 +0000

    Replaces #1 as code changes needed

--- src/main/java/com/masteringapi/attendees/model/Attendee.java
+++ src/main/java/com/masteringapi/attendees/model/Attendee.java
@@ -1,7 +1,8 @@
 package com.masteringapi.attendees.model;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Attendee {
